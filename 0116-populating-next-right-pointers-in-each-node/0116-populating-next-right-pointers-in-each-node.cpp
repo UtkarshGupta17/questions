48fb90b1ba363d930465c1c60d1050c3e367a107
Utkarsh Gupta <103336386+UtkarshGupta17@users.noreply.github.com>
2024-07-10 00:48:30 +0530

    Time: 11 ms (77.71%), Space: 17.6 MB (49.79%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -28,14 +28,14 @@ public:
         while(!q.empty()){
             int size=q.size();
             vector<int>arr2;
-            Node* prev=nullptr;
             for(int i=0;i<size;i++){
                 Node* node=q.front();
                 q.pop();
-                if (prev) {
-                    prev->next = node;
+                if (i < size - 1) {
+                    node->next = q.front();
+                } else {
+                    node->next = NULL;
                 }
-                prev = node;
                 if(node->left){
                     q.push(node->left);
                 }
